5d35b4ed2b5687ec1b252a1636965ae17e9c149a
Antonio RH <arodh91@gmail.com>
2024-12-24 23:14:00 +0100

    Now computes for a specific number of decimals.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 
 int main()
 {
-  const int nDigits = 15;
+  const int userSpecifiedNumberOfDigits = 20;
   MPComputeEngine eng;
   mpf_t output;
   mpf_init(output);
@@ -19,25 +19,28 @@ int main()
 
   mp_exp_t exponent;
 
-// Iterate multiple times to compute the digits of e.
-  for (int i = 1; i <= nDigits; i++)
+  const int precision = 1200;
+  // Iterate multiple times to compute the digits of e.
+  // for (int i = 1; i <= nDigits; i++)
+  int computedNumberOfDigits = 0;
+  int nIterations = 1;
+  while (computedNumberOfDigits < userSpecifiedNumberOfDigits)
   {
     std::cout << ".- ";
 
-    const int precision = 1200;
-    eng.compute(i, precision, output);
+    eng.compute(nIterations, precision, output);
     mpf_out_str(stdout, 10, precision, output);
 
     std::cout << "@ ";
-    if (i != 1)
+    if (nIterations != 1)
     {
       // Subtract the digits of the current and previous iteration.
       mpf_sub(correct_digits, previous_output, output);
-      std::cout << "@";      
+      std::cout << "@";
 
       // Convert such diff in the digits to a string with scientific notation. We will use the exponent value.
-      // I am not completely sure about this, but for a huge amount of digits I think it may be a better idea 
-      // to manipulate this formatted string than a numeric type with a huge mantissa.
+      // I am not completely sure about this, but for a huge amount of digits I think it may be a better idea
+      // to manipulate this formatted string than a numeric type with a huge
       char buffer[2];
       sprintf(buffer, "%.2e", mpf_get_d(correct_digits));
       std::string digits(buffer);
@@ -47,13 +50,19 @@ int main()
       //  Digit    - |X |. |Y |Z |e |- |A |B |
       //  Position - |0 |1 |2 |3 |4 |5 |6 |7 |
       // Also, we cast such quantity to integer.
-      std::cout << "@";      
-      std::cout << digits << ", " << stoi(digits.substr(7, 2));
+      std::cout << "@";
+      computedNumberOfDigits = stoi(digits.substr(7, 2));
+      std::cout << digits << ", " << computedNumberOfDigits;
     }
 
     std::cout << "\n";
     mpf_set(previous_output, output);
+    nIterations++;
   }
+
+  std::cout << "- # iterations: " << nIterations - 1 << "\n- # verified digits: " << computedNumberOfDigits << "\n- e-constant: ";
+  mpf_out_str(stdout, 10, precision, output);
+  std::cout << "\n";
   mpf_clear(output);
   mpf_clear(previous_output);
 }
