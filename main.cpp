8f1f16bc47b3815aeae66e88fff424ff262b8bc0
Antonio RH <arodh91@gmail.com>
2024-12-24 23:40:19 +0100

    Fixed bug in the precision. This happened because I didn't set it in main.cpp at the very beginning, and internal precision was being cut when brought to the main() method and printed.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,12 @@
 
 int main()
 {
-  const int userSpecifiedNumberOfDigits = 20;
+  const int precision = 12000;
+  mpf_set_default_prec(precision);
+  const bool VERBOSITY = false;
+  const bool WRITE_FILE = false;
+
+  const int userSpecifiedNumberOfDigits = 30;
   MPComputeEngine eng;
   mpf_t output;
   mpf_init(output);
@@ -17,26 +22,23 @@ int main()
   mpf_t correct_digits;
   mpf_init(correct_digits);
 
-  mp_exp_t exponent;
-
-  const int precision = 1200;
   // Iterate multiple times to compute the digits of e.
   // for (int i = 1; i <= nDigits; i++)
   int computedNumberOfDigits = 0;
   int nIterations = 1;
   while (computedNumberOfDigits < userSpecifiedNumberOfDigits)
   {
-    std::cout << ".- ";
-
     eng.compute(nIterations, precision, output);
-    mpf_out_str(stdout, 10, precision, output);
+    if (VERBOSITY == true)
+    {
+      std::cout << nIterations << ". ";
+      mpf_out_str(stdout, 1, computedNumberOfDigits, output);
+    }
 
-    std::cout << "@ ";
     if (nIterations != 1)
     {
       // Subtract the digits of the current and previous iteration.
       mpf_sub(correct_digits, previous_output, output);
-      std::cout << "@";
 
       // Convert such diff in the digits to a string with scientific notation. We will use the exponent value.
       // I am not completely sure about this, but for a huge amount of digits I think it may be a better idea
@@ -50,19 +52,33 @@ int main()
       //  Digit    - |X |. |Y |Z |e |- |A |B |
       //  Position - |0 |1 |2 |3 |4 |5 |6 |7 |
       // Also, we cast such quantity to integer.
-      std::cout << "@";
+
       computedNumberOfDigits = stoi(digits.substr(7, 2));
-      std::cout << digits << ", " << computedNumberOfDigits;
+
+      if (VERBOSITY == true)
+      {
+        std::cout << digits << ", " << computedNumberOfDigits;
+        std::cout << "\n";
+      }
     }
 
-    std::cout << "\n";
     mpf_set(previous_output, output);
     nIterations++;
   }
 
+  // Write outputs.
   std::cout << "- # iterations: " << nIterations - 1 << "\n- # verified digits: " << computedNumberOfDigits << "\n- e-constant: ";
-  mpf_out_str(stdout, 10, precision, output);
+  mpf_out_str(stdout, 10, computedNumberOfDigits, output);
   std::cout << "\n";
+
+  // Write the result in a file.
+  if (WRITE_FILE == true)
+  {
+    FILE *pFile;
+    pFile = fopen("out.txt", "w");
+    mpf_out_str(pFile, 10, computedNumberOfDigits, output);
+  }
+
   mpf_clear(output);
   mpf_clear(previous_output);
 }
